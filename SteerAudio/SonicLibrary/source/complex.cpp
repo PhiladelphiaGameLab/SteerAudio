080ff29c759a2d4a6cdea4704618fef0d0882469
Alex Cannon <acannon828@gmail.com>
2014-09-26 17:53:59 -0400

    added Sonic lib as a submodule

--- SteerAudio/SonicLibrary/source/complex.cpp
+++
@@ -1,8 +0,0 @@
-//   complex.cpp - impelementation of class
-//   of complex number
-//
-#include "../include/complex.h"
-
-//   Imaginary unity constants
-const complex complex::i(0., 1.);
-const complex complex::j(0., 1.);
